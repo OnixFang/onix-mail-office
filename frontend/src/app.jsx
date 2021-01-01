bae683592e95daca384ae8ade7ef20c73807dd6e
Ulises Taveras <utaverasperez@firstam.com>
2021-01-01 11:56:05 -0400

    Add initial react-app structure

---
+++ frontend/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+
+const App = (props) => {
+  return (
+    <React.Fragment>
+      <h1>This is the app!</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
