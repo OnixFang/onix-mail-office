5706e72a65723b2336e2c964e8588412022449c8
Ulises Taveras <utaverasperez@firstam.com>
2021-01-01 13:03:34 -0400

    Materialize-css configuration

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import './materialize-src/sass/materialize.scss';
+
+// Styles
+import './theme.scss';
 import './app.scss';
 
 const App = (props) => {
   return (
     <React.Fragment>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">
+            Logo
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <h1>This is the app!</h1>
     </React.Fragment>
   );
