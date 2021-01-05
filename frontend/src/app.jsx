bfaf68c9d917ccae2cc72a16f1fade05a5172745
Ulises Taveras <twily.dev@outlook.com>
2021-01-04 21:23:48 -0400

    Finish Letter appearance

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -6,14 +6,13 @@ import './app.scss';
 
 const App = (props) => {
   return (
-    <div className="row">
-      <div className="col s12 m4 push-m4">
-        <div className="letter-container">
-          <div className="letter">
-            I am a very simple card. I am good at containing small bits of information. I am convenient because I
-            require little markup to use effectively. I am similar to what is called a panel in other frameworks.
-          </div>
-        </div>
+    <div className="letter-container">
+      <div className="letter-edge">&nbsp;</div>
+      <div className="letter">
+        <p>
+          I am a very simple card. I am good at containing small bits of information. I am convenient because I require
+          little markup to use effectively. I am similar to what is called a panel in other frameworks.
+        </p>
       </div>
     </div>
   );
